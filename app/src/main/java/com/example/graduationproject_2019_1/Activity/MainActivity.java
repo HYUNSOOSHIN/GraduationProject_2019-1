42a4f2b007394aeb38d8697ec5fadc83e9cb8e14
HYUNSOOSHIN <shs0655@naver.com>
2019-03-22 01:29:48 +0900

    [안드로이드] MainActivity 틀 수정 / ActionActivity 추가

--- app/src/main/java/com/example/graduationproject_2019_1/Activity/MainActivity.java
+++ app/src/main/java/com/example/graduationproject_2019_1/Activity/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.graduationproject_2019_1.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -121,6 +124,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton nextView_btn = findViewById(R.id.nextView_btn);
+        nextView_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActionActivity.class);
+                startActivity(intent);
+            }
+        });
 
         findUIObjects();
         addBackgroundList();
