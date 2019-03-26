79fe7aa1cd1f061a4d2ee165d23e0d3a80978611
jihwann <jhim000@naver.com>
2019-03-26 23:48:34 +0900

    [안드로이드] RegionCode 추가 완료

---
+++ app/src/main/java/com/example/graduationproject_2019_1/Data/Region.java
@@ -0,0 +1,13 @@
+package com.example.graduationproject_2019_1.Data;
+
+public class Region{
+    public String gu;
+    public String dong;
+    public int code;
+
+    public Region(String gu, String dong, int code){
+        this.gu = gu;
+        this.dong = dong;
+        this.code = code;
+    }
+}
