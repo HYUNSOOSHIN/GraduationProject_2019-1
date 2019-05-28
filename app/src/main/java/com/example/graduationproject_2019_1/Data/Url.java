abcd5c360b3dc9a458cf156ec227136ce6e28fc8
jihwann <jhim000@naver.com>
2019-05-28 11:00:10 +0900

    [서버] 참여게시물 이미지 매핑 / 위젯 디자인 수정

--- app/src/main/java/com/example/graduationproject_2019_1/Data/Url.java
+++ app/src/main/java/com/example/graduationproject_2019_1/Data/Url.java
@@ -3,5 +3,6 @@ package com.example.graduationproject_2019_1.Data;
 public class Url {
     public static final String REAL_TIME_CITY_AIR = "http://openapi.seoul.go.kr:8088/61446d4e496a686932366773527076/json/RealtimeCityAir/1/5/";
 //    public static final String SPRING_SERVER = "http://172.16.2.232:8080/hansung"; //폰 연결해서 사용
-    public static final String SPRING_SERVER = "http://10.0.2.2:8080/hansung"; //안드로이드 에뮬레이터에서 사용
+    //public static final String SPRING_SERVER = "http://10.0.2.2:8080/hansung"; //안드로이드 에뮬레이터에서 사용
+    public static final String SPRING_SERVER = "http://13.125.245.98:8080/fcm"; //안드로이드 에뮬레이터에서 사용
 }
